c6733264932eed414f9ce728f6c8e22f44c40e72
salahuddinmd2244-lang <salahuddinmd2244@gmail.com>
2026-02-19 11:43:36 +0530

    tag

--- vnet.tf
+++ vnet.tf
@@ -3,5 +3,9 @@ resource "azurerm_virtual_network" "vnet" {
     address_address_space = ["10.1.0.0/16"]
     resource_group_name = azurerem_resource_group.rg.name
     location = azurerm_resource_group.rg.name
+    tags = {
+      owner = devenv
+    
+    }
 
 }
