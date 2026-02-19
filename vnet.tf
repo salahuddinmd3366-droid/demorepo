1fd97705d258ebcc51de51b5e3e88d023527774c
salahuddinmd2244-lang <salahuddinmd2244@gmail.com>
2026-02-19 11:03:03 +0530

    vnet

---
+++ vnet.tf
@@ -0,0 +1,7 @@
+resource "azurerm_virtual_network" "vnet" {
+    name = "vnet"
+    address_address_space = ["10.1.0.0/16"]
+    resource_group_name = azurerem_resource_group.rg.name
+    location = azurerm_resource_group.rg.name
+
+}
